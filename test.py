8428aa645f6b286271daa6bb5ee92b16d44ca3b0
SnakeHisoka <94120737+SnakeHisoka@users.noreply.github.com>
2021-11-29 22:57:33 +0400

    29-11

--- test.py
+++ test.py
@@ -1,25 +1,14 @@
-# Задание-3:
-# Дан файл ("data/fruits") со списком фруктов.
-# Записать в новые файлы все фрукты, начинающиеся с определенной буквы.
-# Т.е. в одном файле будут все фрукты на букву “А”, во втором на “Б” и т.д.
-# Файлы назвать соответственно.
-# Пример имен файлов: fruits_А, fruits_Б, fruits_В ….
-# Важно! Обратите внимание, что нет фруктов, начинающихся с некоторых букв.
-# Напишите универсальный код, который будет работать с любым списком фруктов
-# и распределять по файлам в зависимости от первых букв, имеющихся в списке фруктов.
-# Подсказка:
-# Чтобы получить список больших букв русского алфавита:
-# print(list(map(chr, range(ord('А'), ord('Я')+1))))
 
-import re
-dikt_fruts = dict()
-with open('C:/HomeWork/lesson 4/Файлы/frukt.txt',encoding='utf-8') as inp_ut:
-    for fruits in inp_ut.readlines():
-        file_name = 'C:/HomeWork/lesson 4/Файлы/Фрукты Результат/frukt_{}'.format(fruits[0].upper())
-        dikt_fruts[file_name] = dikt_fruts.get(file_name,'')+fruits
-    
-for i in dikt_fruts:
-    name = '{}.txt'.format(i)
-    with open(name,'w') as out:
-        out.write(dikt_fruts[i])
-print('Формирование файлов по именам фруктов закончено!')
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+y = [5, 3, 4, 1, 2, 6 ,7 ,8]
+c = 0
+for i in range(7):
+    x1 = x[i]
+    y1 = y[i]
+    for j in range(i+1, 8):
+        x2 = x[j]
+        y2 = y[j]
+        if x1 == x2 or y1 == y2 or (abs(x1 - x2) == abs(y1 - y2)):
+            print('YES')
+            exit()
+print('NO')
